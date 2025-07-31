a70a6ce93458a83c4fb9ec7ecca83abbe8af4f85
Mighty8H <555centy@gmail.com>
2025-07-31 10:08:56 +0200

    refactored to English

--- cs2_weapon_randomizer.cpp
+++ cs2_weapon_randomizer.cpp
@@ -4,39 +4,38 @@
 #include <string>
 #include <random>
 #include <ctime>
+#include <memory>
 
 /*
 TODO: 
 - 1/1000 zeus chance instead of primary
 - make header file
-- fix function los_bronii_oddzielnie
-- add function los_druzyn
+- fix function rand_weapons_separatly
+- add function rand_teams
+- refactor to english
 */
 
 using std::string;
 
-uint16_t ilosc_graczy = 0;
-uint16_t temp_ilosc_graczy;
-const uint8_t dlugosc_nazwy = 255;
-char* temp_text = new char[dlugosc_nazwy];
-std::vector<Gracz> gracze;
-string str_ilosc_graczy;
-string n;
+uint16_t num_players = 0;
+uint16_t temp_num_players;
+const uint8_t name_length = 255;
+std::vector<Player> players;
 
-//dane aktualne na dzien 30.07.2025
-const string terro_pistolety[7] = { "Glock-18", "Dual Berettas", "P250", "Tec-9", "CZ75-Auto", "Desert Eagle", "R8 Revolver" };
-const string ct_pistolety[8] = { "P2000", "USP-S", "Dual Berettas", "P250", "Five-SeveN", "CZ75-Auto", "Desert Eagle", "R8 Revolver" };
-const string terro_bronie_glowne[17] = { "MAC-10", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon", "Nova", "XM1014", "Sawed-Off", "M249", "Negev", "Galil AR", "AK-47", "SSG 08", "SG 553", "AWP", "G3SG1" };
-const string ct_bronie_glowne[18] = { "MP9", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon", "Nova", "XM1014", "MAG-7", "M249", "Negev", "FAMAS", "M4A4", "M4A1-S", "SSG 08", "AUG", "AWP", "SCAR-20" };
-const string mapy[13] = { "Dust II", "Mirage", "Vertigo", "Ancient", "Inferno", "Nuke", "Anubis", "Overpass", "Jura", "Grail", "Agency", "Office", "Italy" };
+//data up to date as of 30.07.2025
+const string terro_secondary[7] = { "Glock-18", "Dual Berettas", "P250", "Tec-9", "CZ75-Auto", "Desert Eagle", "R8 Revolver" };
+const string ct_secondary[8] = { "P2000", "USP-S", "Dual Berettas", "P250", "Five-SeveN", "CZ75-Auto", "Desert Eagle", "R8 Revolver" };
+const string terro_primary[17] = { "MAC-10", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon", "Nova", "XM1014", "Sawed-Off", "M249", "Negev", "Galil AR", "AK-47", "SSG 08", "SG 553", "AWP", "G3SG1" };
+const string ct_primary[18] = { "MP9", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon", "Nova", "XM1014", "MAG-7", "M249", "Negev", "FAMAS", "M4A4", "M4A1-S", "SSG 08", "AUG", "AWP", "SCAR-20" };
+const string maps[13] = { "Dust II", "Mirage", "Vertigo", "Ancient", "Inferno", "Nuke", "Anubis", "Overpass", "Jura", "Grail", "Agency", "Office", "Italy" };
 
-struct Gracz {
-    string nazwa;
-    string terro_pistolet;
-    string ct_pistolet;
-    string terro_bron_glowna;
-    string ct_bron_glowna;
-    string druzyna;
+struct Player {
+    string name;
+    string terro_secondary;
+    string ct_secondary;
+    string terro_primary;
+    string ct_primary;
+    string team;
 };
 
 bool in_array(const std::string& value, const std::vector<std::string>& array) {
@@ -45,169 +44,155 @@ bool in_array(const std::string& value, const std::vector<std::string>& array) {
 
 bool is_integer(const std::string& str) {
     try {
-        temp_ilosc_graczy = std::stoi(str);
+        temp_num_players = std::stoi(str);
         return false;
     }
     catch(const std::invalid_argument& e) {
-        std::cerr << "podany napis nie jest liczba" << "\n";
+        std::cerr << "entered string isn't a number" << "\n";
         return true;
     }
 }
 
-void dodaj_graczy() {
-    do {
-        std::cout << "Podaj ilosc graczy ";
-        std::cin >> str_ilosc_graczy;
-    }
-    while(is_integer(str_ilosc_graczy));
+int get_random_index(int min, int max) {
+    static std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<> dist(min, max);
+    return dist(gen);
+}
 
-    if(gracze.size() == 0)
-        std::vector<Gracz> gracze(temp_ilosc_graczy);
+void add_players() {
+    string str_num_players;
+
+    do {
+        std::cout << "Enter number of players ";
+        std::cin >> str_num_players;
+    }
+    while(is_integer(str_num_players));
+
+    if(players.size() == 0)
+        std::vector<Player> players(temp_num_players);
 
     else {
-        for(size_t i = 0; i < temp_ilosc_graczy; i++)
-            gracze.emplace_back();
+        for(size_t i = 0; i < temp_num_players; i++)
+            players.emplace_back();
     }
 
-    ilosc_graczy += temp_ilosc_graczy;
+    num_players += temp_num_players;
     std::cin.ignore();
 }
 
-void los_bronii_oddzielnie() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist_terro_pistolety(0, 6);
-    std::uniform_int_distribution<> dist_ct_pistolety(0, 7);
-    std::uniform_int_distribution<> dist_terro_bronie_glowne(0, 16);
-    std::uniform_int_distribution<> dist_ct_bronie_glowne(0, 17);
-
-    do {
-        std::cout << "podaj ilosc graczy ";
-        std::cin >> str_ilosc_graczy;
-    }
-    while(is_integer(str_ilosc_graczy));
-	
-    //losowanie broni dla graczy
-    for(size_t i = 0; i < ilosc_graczy; i++) {
-        std::cout << "\npodaj nazwe gracza ";
+void rand_weapons_separately() {
+    std::unique_ptr<char[]> temp_text(new char[name_length]);
+    
+    //randomising weapons for players
+    for(size_t i = 0; i < num_players; i++) {
+        std::cout << "\nEnter player No." << i+1 << " name ";
         
-        std::cin.getline(temp_text, dlugosc_nazwy);
-        gracze[i].nazwa = (temp_text ? temp_text : "");
+        std::cin.getline(temp_text.get(), name_length);
+        players[i].name = (temp_text.get() ? temp_text.get() : "");
 
-        gracze[i].terro_pistolet = terro_pistolety[dist_terro_pistolety(gen)];
-        gracze[i].ct_pistolet = ct_pistolety[dist_ct_pistolety(gen)];
-        gracze[i].terro_bron_glowna = terro_bronie_glowne[dist_terro_bronie_glowne(gen)];
-        gracze[i].ct_bron_glowna = ct_bronie_glowne[dist_ct_bronie_glowne(gen)];
+        players[i].terro_secondary = terro_secondary[get_random_index(0, 6)];
+        players[i].ct_secondary = ct_secondary[get_random_index(0, 7)];
+        players[i].terro_primary = terro_primary[get_random_index(0, 16)];
+        players[i].ct_primary = ct_primary[get_random_index(0, 17)];
     }
 
     std::cout << "\n";
 
-    //wypisywanie
-    for(size_t i = 0; i < ilosc_graczy; i++) {
-        std::cout << gracze[i].nazwa << ": \nterro: " << gracze[i].terro_pistolet << ", " << gracze[i].terro_bron_glowna
-                                     << "\nct: " << gracze[i].ct_pistolet << ", " << gracze[i].ct_bron_glowna << "\n\n";
+    //printing
+    for(size_t i = 0; i < num_players; i++) {
+        std::cout << players[i].name << ": \nterro: " << players[i].terro_secondary << ", " << players[i].terro_primary
+                                     << "\nct: " << players[i].ct_secondary << ", " << players[i].ct_primary << "\n\n";
     }
 }
 
-void los_bronii_razem() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist_terro_pistolety(0, 6);
-    std::uniform_int_distribution<> dist_ct_pistolety(0, 7);
-    std::uniform_int_distribution<> dist_terro_bronie_glowne(0, 16);
-    std::uniform_int_distribution<> dist_ct_bronie_glowne(0, 17);
-    
-    string los;
+void rand_weapons_together() {
+    string rand_gun;
     std::vector<string> terro(5);
     std::vector<string> ct(5);
 
-    //losowanie broni dla terro
-    terro[0] = terro_pistolety[dist_terro_pistolety(gen)];
+    //randomising weapons for terro
+    terro[0] = terro_secondary[get_random_index(0, 6)];
     while(terro[1].empty()) {
-        los = terro_pistolety[dist_terro_pistolety(gen)];
+        rand_gun = terro_secondary[get_random_index(0, 6)];
 
-        if(!in_array(los, terro)) {
-            terro[1] = los;
+        if(!in_array(rand_gun, terro)) {
+            terro[1] = rand_gun;
         }
     }
 
-    terro[2] = terro_bronie_glowne[dist_terro_bronie_glowne(gen)];
+    terro[2] = terro_primary[get_random_index(0, 16)];
     while(terro[3].empty() || terro[4].empty()) {
-        los = terro_bronie_glowne[dist_terro_bronie_glowne(gen)];
+        rand_gun = terro_primary[get_random_index(0, 16)];
 
-        if(terro[3].empty() && !in_array(los, terro)) {
-            terro[3] = los;
+        if(terro[3].empty() && !in_array(rand_gun, terro)) {
+            terro[3] = rand_gun;
         }
-        else if(!in_array(los, terro)) {
-            terro[4] = los;
+        else if(!in_array(rand_gun, terro)) {
+            terro[4] = rand_gun;
         }
     }
 
-    //losowanie broni dla ct
-    ct[0] = ct_pistolety[dist_ct_pistolety(gen)];
+    //randomising weapons for ct
+    ct[0] = ct_secondary[get_random_index(0, 7)];
     while(ct[1].empty()) {
-        los = ct_pistolety[dist_ct_pistolety(gen)];
+        rand_gun = ct_secondary[get_random_index(0, 7)];
 
-        if(!in_array(los, ct)) {
-            ct[1] = los;
+        if(!in_array(rand_gun, ct)) {
+            ct[1] = rand_gun;
         }
     }
 
-    ct[2] = ct_bronie_glowne[dist_ct_bronie_glowne(gen)];
+    ct[2] = ct_primary[get_random_index(0, 17)];
     while(ct[3].empty() || ct[4].empty()) {
-        los = ct_bronie_glowne[dist_ct_bronie_glowne(gen)];
+        rand_gun = ct_primary[get_random_index(0, 17)];
 
-        if(ct[3].empty() && !in_array(los, ct)) {
-            ct[3] = los;
+        if(ct[3].empty() && !in_array(rand_gun, ct)) {
+            ct[3] = rand_gun;
         }
-        else if(!in_array(los, ct)) {
-            ct[4] = los;
+        else if(!in_array(rand_gun, ct)) {
+            ct[4] = rand_gun;
         }
     }
     
-    //wypisywanie
-    std::cout << "terro: " << terro[0] << ", " << terro[1] << "; " << terro[2] << ", " << terro[3] << ", " << terro[4]
-              << "\nct: " << ct[0] << ", " << ct[1] << "; " << ct[2] << ", " << ct[3] << ", " << ct[4] << "\n";
+    //printing
+    std::cout << "terro: " << terro[0] << ", " << terro[1] << "; " << terro[2] << ", " << terro[3] << ", " << terro[4] << "\n"
+              << "ct: " << ct[0] << ", " << ct[1] << "; " << ct[2] << ", " << ct[3] << ", " << ct[4] << "\n";
     
 }
 
-void los_druzyn() {
+void rand_team() {
 
 }
 
-void los_mapy() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist_mapy(0, 12);
-    std::cout << mapy[dist_mapy(gen)] << "\n";
+void rand_maps() {
+    std::cout << maps[get_random_index(0, 12)] << "\n";
 }
 
 int main() {
+    string n;
     while(true) {
-        std::cout << "Wybierz opcje(0 - wylacz program; 1 - dodaj graczy; 2 - losowanie bronii dla kazdego oddzielnie; 3 - losowanie bronii dla wszystkich razem; 4 - losowanie druzyn; 5 - losowanie mapy)\n";
+        std::cout << "Choose option(0 - exit program; 1 - add players; 2 - randomise weapons for everyone separately; 3 - randomise weapons for everyone together; 4 - randomise team; 5 - randomise map)\n";
         std::cin >> n;
 
         if(n.compare("0") == 0) {
-            delete[] temp_text;
-            temp_text = nullptr;
             return 0;
         }
         else if(n.compare("1") == 0)
-            dodaj_graczy();
+            add_players();
 
         else if(n.compare("2") == 0)
-            los_bronii_oddzielnie();
+            rand_weapons_separately();
 
         else if(n.compare("3") == 0)
-            los_bronii_razem();
+            rand_weapons_together();
             
         else if(n.compare("4") == 0)
-            los_druzyn();
+            rand_team();
             
         else if(n.compare("5") == 0)
-            los_mapy();
+            rand_maps();
 
         else
-            std::cerr << "nieprawidlowa opcja\n";
+            std::cerr << "invalid option\n";
     }
 }
